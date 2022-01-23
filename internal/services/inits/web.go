dbee6ae9feee057c4eca74765ffd647382cb6439
vivivdsystem <notrealbutnotobot@gmail.com>
2022-01-23 18:41:36 +0100

    improved object creation of the fiber server

--- internal/services/inits/web.go
+++ internal/services/inits/web.go
@@ -1,13 +1,21 @@
 package inits
 
 import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
 	"github.com/meoera/doorman/internal/services/config"
 	"github.com/meoera/doorman/internal/services/database"
-	"github.com/meoera/doorman/internal/services/web"
 	"github.com/meoera/doorman/internal/services/web/routes"
 )
 
-func Web(cfg *config.Web, db database.Database, cacheDb database.CacheDatabase, devMode bool) (err error) {
-	routes.Add(cfg, db, cacheDb, devMode)
-	return web.Server.Listen(cfg.Host)
+func Web(cfg *config.Web, db database.Database, cacheDb database.CacheDatabase) (err error) {
+
+	app := fiber.New(fiber.Config{
+		ErrorHandler: func(c *fiber.Ctx, e error) error {
+			return fmt.Errorf("An error occured: %v\n The context: %v", e, c)
+		},
+	})
+	routes.Add(app, cfg, db, cacheDb)
+	return app.Listen(cfg.Host)
 }
