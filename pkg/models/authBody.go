1682a4fd01a63d6c2064bf0aa4207acec6fe30f0
vivivdsystem <notrealbutnotobot@gmail.com>
2022-01-23 17:26:00 +0100

    added RefreshRequestBody

--- pkg/models/authBody.go
+++ pkg/models/authBody.go
@@ -1,6 +1,10 @@
 package models 
 
-type AuthRequestBody struct {
+type AuthenticationRequestBody struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type RefreshRequestBody struct {
+	RefreshToken string `json:"refresh_token"`
 }
